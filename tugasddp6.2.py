6d5dd51560e9b867bc687c4c95e048e06f25492e
Dini Mufidah <qiroatyalquds@gmail.com>
2023-11-05 16:54:27 +0700

    first commit

---
+++ tugasddp6.2.py
@@ -0,0 +1,11 @@
+print ('tugas2')
+print  ('')
+
+a=1
+b=0
+while (a<=19):
+    b+=a
+    a+=2
+print('1+3+5+7+9+11+13+15+17+19=',b)
+
+print("\n")
---
+++ tugasddp6.3.py
@@ -0,0 +1,5 @@
+for x in range(0,4):
+    for y in range (0,4):
+        if x > y:
+            print('*',end='')
+    print()        
